d9f22f0c02d6479cada862c294f2fa6b4edd6813
Michael Soares <mikey.soares@gmail.com>
2019-01-10 13:20:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ if line.length == 0
   puts "The line is currently empty."
 else
   string = ["The line is currently: "]
-  line.each_with_index do |name, index|
+  line.each_with_index { |name, index|
     string << "#{index + 1}. #{name}"
+  }
     puts string.join
   end
 
